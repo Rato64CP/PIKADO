80deb63b9c480b2bf6aaaf1b130ff11cc083ec9c
Rato64CP <ratomir.vukorepa@gmail.com>
2025-07-19 15:28:52 +0200

    Update PIKADO.ino

--- PIKADO.ino
+++ PIKADO.ino
@@ -1,1 +1,13 @@
+#include "config.h"
+#include "detection.h"
 
+void setup() {
+  Serial.begin(9600);
+  inicijalizirajMete();
+}
+
+void loop() {
+  skenirajMete();
+  detektirajPromasaj();
+  delay(10);  // mala pauza da rastereti petlju
+}
