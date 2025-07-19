48b5d06ec259670da07716c97cb1e499835b1213
Rato64CP <ratomir.vukorepa@gmail.com>
2025-07-19 15:19:01 +0200

    Create config.h

---
+++ config.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <Arduino.h>
+
+// Zajednički pinovi
+constexpr uint8_t COM_45 = 45;
+constexpr uint8_t COM_47 = 47;
+constexpr uint8_t COM_A1 = A1;
+constexpr uint8_t COM_A2 = A2;
+
+// Pin za mikrofon
+constexpr uint8_t PIN_MIKROFON = A0;
+constexpr int THRESHOLD_PROMASAJ = 600;
