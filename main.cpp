5b8a2ca97dcc8377ac2db3d272646a34e7ccf394
Rato64CP <ratomir.vukorepa@gmail.com>
2025-07-19 15:20:43 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include "config.h"
+#include "detection.h"
+
+void setup() {
+  Serial.begin(9600);
+  inicijalizirajMete();
+}
+
+void loop() {
+  skenirajMete();
+  detektirajPromasaj();
+  delay(10);
+}
