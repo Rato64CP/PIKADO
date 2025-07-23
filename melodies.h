fbae9f15a0ca142a371f3a10420be7b8cebdeefa
Rato64CP <ratomir.vukorepa@gmail.com>
2025-07-23 09:48:34 +0200

    Add bust sound for countdown games

--- melodies.h
+++ melodies.h
@@ -9,5 +9,6 @@ void svirajZvukVadenja();
 void svirajZvukPobjede();
 void svirajZvukTipke();
 void svirajZvukNepostavljenaIgra();
+void svirajZvukBust();
 // Pusti MP3 datoteku sa snimljenim imenom igrača (0-5)
 void svirajImeIgraca(uint8_t index);
