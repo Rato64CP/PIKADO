e61a6607f6172539fafd61993344c24445abc29c
Rato64CP <ratomir.vukorepa@gmail.com>
2025-07-29 17:35:31 +0200

    Improve game and player selection prompts

--- src/modules/lcd_display.h
+++ src/modules/lcd_display.h
@@ -4,3 +4,5 @@
 void inicijalizirajLCD();
 void ispisiNaLCD(const String &poruka);
 void logPoruka(const String &poruka);
+void ispisiDvijeLinije(const String &red1, const String &red2);
+void logPorukaDvijeLinije(const String &r1, const String &r2);
